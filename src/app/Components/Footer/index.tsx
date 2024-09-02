f6b72930e0e722372809db0c3624475595f8e946
DIVIYA <kdmail9@gmail.com>
2024-09-02 10:46:02 +0530

    comit footer change

--- src/app/Components/Footer/index.tsx
+++ src/app/Components/Footer/index.tsx
@@ -3,12 +3,14 @@ import footer from "./assets/Footer (10).png";
 
 const Footer = () => {
   return (
-    <footer className="relative w-full h-[300px]"> {/* Adjust the height as needed */}
+    <footer className="relative w-full h-[200px] md:h-[300px]"> {/* Adjust the height for responsiveness */}
       <Image
         src={footer}
         alt="Footer"
-        layout="fill" // This will make the image fill the container
+        layout="fill" // This makes the image fill the container
         objectFit="cover" // Ensures the image covers the area without stretching
+        quality={100} // Set the quality of the image
+        priority // Load the image with high priority for better performance
         className="w-full h-full"
       />
     </footer>
