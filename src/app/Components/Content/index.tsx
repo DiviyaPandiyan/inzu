b993db798e1fc3c94663ad1974aae4143f01ae3e
DIVIYA <kdmail9@gmail.com>
2024-09-02 09:56:25 +0530

    commit error clear

--- src/app/Components/Content/index.tsx
+++ src/app/Components/Content/index.tsx
@@ -45,7 +45,7 @@ const ContentPage = () => {
                 id="countries"
                 className="bg-[#FFFFFF] shadow-md border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               >
-                <option defaultValue>Choose a country</option>
+                <option >Choose a country</option>
                 <option value="US">United States</option>
                 <option value="CA">Canada</option>
                 <option value="FR">France</option>
